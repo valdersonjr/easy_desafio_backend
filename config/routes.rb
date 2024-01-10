da9e6cc14b34783132b8a2cba3f3bad82ebf602c
valdersonjr <valdersonpontes@gmail.com>
2024-01-10 05:16:02 -0300

    :bug: [FIX] Routes not recognizing sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users, path: '', path_names: {
-    sign_in: 'login',
-    sign_out: 'logout',
-    registration: 'signup'
-  },
-  controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations'
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
   }
+
+  devise_scope :user do
+    post '/users/sign_up' => 'users/registrations#create'
+    
+    post '/users/sign_in' => 'users/sessions#create'
+    delete '/users/sign_out' => 'users/sessions#destroy'
+  end
 end
