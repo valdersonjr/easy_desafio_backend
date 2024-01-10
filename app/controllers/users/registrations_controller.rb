6af296ddcd4046f827c52a737588477f28518f2d
valdersonjr <valdersonpontes@gmail.com>
2024-01-10 07:14:36 -0300

    :recycle: [UPDATE] Refactor devise user signup

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -12,9 +12,23 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    build_resource(sign_up_params)
+    resource.save
+
+    if resource.persisted?
+      if resource.active_for_authentication?
+        sign_up(resource_name, resource)
+        render json: { code: 200, message: 'Signed up successfully', data: resource }, status: :ok
+      else
+        expire_data_after_sign_in!
+        render json: { result: 'success_with_notice', notice: "signed_up_but_#{resource.inactive_message}" }
+      end
+    else
+      render json: { message: 'User could not be created successfully', errors: resource.errors.full_messages },
+             status: :unprocessable_entity
+    end
+  end
 
   # GET /resource/edit
   # def edit
