6e927f90d54fd8a385e894cb2df53b9b14b7cc25
valdersonjr <valdersonpontes@gmail.com>
2024-01-14 16:08:00 -0300

    :white_check_mark: [ADD] User model tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
 
+  # validates 
+  validates :email, presence: true
+  validates :password, presence: true, on: %i[create update]
   validates :password_confirmation, presence: true, on: %i[create update]
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Devise modules' do
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password) }
+    it { should validate_presence_of(:password_confirmation).on(:create) }
+    it { should validate_presence_of(:password_confirmation).on(:update) }
+    it { should validate_uniqueness_of(:email).case_insensitive }
+    it { should validate_length_of(:password).is_at_least(Devise.password_length.min).is_at_most(Devise.password_length.max).on(:create) }
+    it { should have_db_index(:email).unique }
+  end
+
+  describe 'Devise JWT' do
+    it { should respond_to(:jwt_payload) }
+    it { should respond_to(:jwt_subject) }
+  end
 end
