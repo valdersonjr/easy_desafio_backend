3dc05d45944d83e2e48e5e81f0072b644f2885de
valdersonjr <valdersonpontes@gmail.com>
2024-01-16 09:51:24 -0300

    :white_check_mark: [ADD] Product request tests

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,68 +1,83 @@
 class ProductsController < ApplicationController
-    before_action :authenticate_user!
+  before_action :authenticate_user!
 
-    def index
-        @products = Product.all
-        if @products.any?
-            render json: ProductSerializer.new(@products).serializable_hash
-        else
-            render json: { message: 'There is no existing product' }, status: :ok
-        end
+  def index
+    @products = Product.all
+    if @products.any?
+      render json: { products: ProductSerializer.new(@products).serializable_hash }, status: :ok
+    else
+      render json: { message: 'There is no existing product' }, status: :not_found
+    end
+  end
+
+  def show
+    @product = Product.find_by(id: params[:id])
+
+    if @product
+      render json: ProductSerializer.new(@product).serializable_hash
+    else
+      render json: { error: 'Product not found' }, status: :not_found
+    end
+  end
+
+  def create
+    if current_user.profile != 'admin'
+      render json: { error: 'You are not allowed to create a product' }, status: :forbidden
+      return
     end
 
-    def show
-        @product = Product.find_by(id: params[:id])
-        
-        if @product
-            render json: ProductSerializer.new(@product).serializable_hash
-        else
-            render json: { error: 'Product not found' }, status: :not_found
-        end
+    @product = Product.new
+    @product.attributes = product_params
+    save_product!
+  end
+
+  def update
+    if current_user.profile != 'admin'
+      render json: { error: 'You are not allowed to update a product' }, status: :forbidden
+      return
     end
 
-    def create
-        @product = Product.new
-        @product.attributes = product_params
-        save_product!
+    @product = Product.find_by(id: params[:id])
+
+    if @product
+      @product.update(product_params)
+      render json: ProductSerializer.new(@product).serializable_hash
+    else
+      render json: { error: 'Product not found' }, status: :not_found
+    end
+  rescue StandardError => e
+    render json: { error: "An error occurred: #{e.message}" }, status: :internal_server_error
+  end
+
+  def destroy
+    if current_user.profile != 'admin'
+      render json: { error: 'You are not allowed to create a product' }, status: :forbidden
+      return
     end
 
-    def update
-        @product = Product.find_by(id: params[:id])
+    @product = Product.find_by(id: params[:id])
 
-        if @product
-            @product.update(product_params)
-            render json: ProductSerializer.new(@product).serializable_hash
-        else
-            render json: { error: 'Product not found' }, status: :not_found
-        end
-    rescue
-        render_error(fields: @product.errors.messages)
+    if @product
+      @product.destroy
+      render json: { message: 'Product successfully deleted' }, status: :ok
+    else
+      render json: { error: 'Product not found' }, status: :not_found
     end
+  rescue StandardError => e
+    render json: { error: "An error occurred: #{e.message}" }, status: :internal_server_error
+  end
 
-    def destroy
-        @product = Product.find_by(id: params[:id])
-      
-        if @product
-          @product.destroy
-          render json: { message: 'Product successfully deleted' }, status: :ok
-        else
-          render json: { error: 'Product not found' }, status: :not_found
-        end
-    rescue
-        render_error(fields: @product.errors.messages)
-    end
+  private
 
-    private
+  def product_params
+    return {} unless params.has_key?(:product)
+    params.require(:product).permit(:name, :ballast)
+  end
 
-    def product_params
-        return {} unless params.has_key?(:product)
-        params.require(:product).permit(:name, :ballast)
-    end
-
-    def save_product! 
-        @product.save!
-        render json: ProductSerializer.new(@product).serializable_hash
-    rescue
-        render_error(fields: @product.errors.messages)
-    end
+  def save_product!
+    @product.save!
+    render json: ProductSerializer.new(@product).serializable_hash
+  rescue StandardError => e
+    render json: { error: "An error occurred: #{e.message}" }, status: :internal_server_error
+  end
 end
--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -1,7 +1,146 @@
 require 'rails_helper'
 
 RSpec.describe "Products", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let(:user) { create(:user, profile: 'client') }
+  let(:admin_user) { create(:user, profile: 'admin') }
+  let(:product) { create(:product) }
+
+  describe "GET /products" do
+    before do
+      set_auth_headers(user)
+    end
+
+    it 'returns http success' do
+      create_list(:product, 1)
+      get '/products', headers: @auth_headers
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'returns not found status' do
+      get '/products', headers: @auth_headers
+      expect(response).to have_http_status(:not_found)
+    end
+
+    it 'returns a list of products' do
+      create_list(:product, 10)
+      get '/products', headers: @auth_headers
+      expect(body_json(response)['products'].count).to eq(10)
+    end
+  end
+
+  describe "GET /product/:id" do
+    before do
+      set_auth_headers(user)
+    end
+
+    it 'returns http success' do
+      product = create(:product)
+      get "/product/#{product.id}", headers: @auth_headers
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'returns not found status' do
+      get '/product/0', headers: @auth_headers
+      expect(response).to have_http_status(:not_found)
+    end
+
+    it 'returns a product' do
+      product = create(:product)
+      get "/product/#{product.id}", headers: @auth_headers
+      expect(body_json(response)['id'].to_i).to eq(product.id)
+    end
+  end
+
+  describe "POST /product/add" do
+    context 'with admin user' do
+      before do
+        set_auth_headers(admin_user)
+      end
+
+      it 'returns http success' do
+        post '/product/add', params: { product: attributes_for(:product) }, headers: @auth_headers	
+        expect(response).to have_http_status(:ok)
+      end
+  
+      it 'returns a product' do
+        post '/product/add', params: { product: attributes_for(:product) }, headers: @auth_headers	
+        expect(body_json(response)['id'].to_i).to eq(Product.last.id)
+      end
+    end
+
+    context 'with client user' do
+      before do
+        set_auth_headers(user)
+      end
+
+      it 'returns forbidden status' do
+        post '/product/add', params: { product: attributes_for(:product) }, headers: @auth_headers	
+        expect(response).to have_http_status(:forbidden)
+      end
+    end
+  end
+
+  describe "PUT /product/edit/:id" do
+    context 'with admin user' do
+      before do
+        set_auth_headers(admin_user)
+      end
+
+      it 'returns http success' do
+        product = create(:product)
+        put "/product/edit/#{product.id}", params: { product: attributes_for(:product) }, headers: @auth_headers	
+        expect(response).to have_http_status(:ok)
+      end
+  
+      it 'returns a product' do
+        product = create(:product)
+        put "/product/edit/#{product.id}", params: { product: attributes_for(:product) }, headers: @auth_headers	
+        expect(body_json(response)['id'].to_i).to eq(product.id)
+      end
+    end
+
+    context 'with client user' do
+      before do
+        set_auth_headers(user)
+      end
+
+      it 'returns forbidden status' do
+        product = create(:product)
+        put "/product/edit/#{product.id}", params: { product: attributes_for(:product) }, headers: @auth_headers	
+        expect(response).to have_http_status(:forbidden)
+      end
+    end
+  end
+
+  describe "DELETE /product/delete/:id" do
+    context 'with admin user' do
+      before do
+        set_auth_headers(admin_user)
+      end
+
+      it 'returns http success' do
+        product = create(:product)
+        delete "/product/delete/#{product.id}", headers: @auth_headers	
+        expect(response).to have_http_status(:ok)
+      end
+  
+      it 'returns a message' do
+        product = create(:product)
+        delete "/product/delete/#{product.id}", headers: @auth_headers	
+        expect(body_json(response)['message']).to eq('Product successfully deleted')
+      end
+    end
+
+    context 'with client user' do
+      before do
+        set_auth_headers(user)
+      end
+
+      it 'returns forbidden status' do
+        product = create(:product)
+        delete "/product/delete/#{product.id}", headers: @auth_headers	
+        expect(response).to have_http_status(:forbidden)
+      end
+    end
   end
 end
