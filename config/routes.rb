c4210c03cc732f228f6092552b3933970f49d0b5
valdersonjr <valdersonpontes@gmail.com>
2024-01-25 05:44:38 -0300

    :recycle: [UPDATE] Routes file changed to a more cleaner form of writing

--- config/routes.rb
+++ config/routes.rb
@@ -4,27 +4,7 @@ Rails.application.routes.draw do
     sessions: 'users/sessions'
   }
 
-  devise_scope :user do
-    post '/user/sign_up' => 'users/registrations#create'
-    put '/user/edit(/:id)' => 'users/registrations#update'
-    delete '/user/delete' => 'users/registrations#destroy'
-
-    post '/user/sign_in' => 'users/sessions#create'
-    delete '/user/sign_out' => 'users/sessions#destroy'
-  end
-
-  get '/users' => 'users/users#index'
-  get '/users/:id' => 'users/users#show'
-
-  get '/products' => 'products#index'
-  get '/product/:id' => 'products#show'
-  post '/product/add' => 'products#create'
-  put '/product/edit/:id' => 'products#update'
-  delete '/product/delete/:id' => 'products#destroy'
-
-  get '/loads' => 'loads#index'
-  get '/load/:id' => 'loads#show'
-  post '/load/add' => 'loads#create'
-  put '/load/edit/:id' => 'loads#update'
-  delete '/load/delete/:id' => 'loads#destroy'
+  resources :users, path: '', only: [:index, :show]
+  resources :products
+  resources :orders
 end
